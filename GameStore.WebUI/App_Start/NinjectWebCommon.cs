a1097b7fe64bfdde9beedae5e0444becc1281bc3
Alex <licoms1997@gmail.com>
2023-08-16 21:24:38 +0300

    Modify RegisterServices method

--- GameStore.WebUI/App_Start/NinjectWebCommon.cs
+++ GameStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -53,6 +53,8 @@ namespace GameStore.WebUI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            System.Web.Mvc.DependencyResolver.SetResolver(new
+                GameStore.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
         }        
     }
 }
