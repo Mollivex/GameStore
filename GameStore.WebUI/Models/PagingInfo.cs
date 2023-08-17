a0421eb724458dcd5844f74dc2c4be10b56cf087
Alex <licoms1997@gmail.com>
2023-08-17 21:32:33 +0300

    Add PagingInfo view model in /GameStore.WebUI/Models/

---
+++ GameStore.WebUI/Models/PagingInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.WebUI.Models
+{
+    public class PagingInfo
+    {
+            // Products total number
+        public int TotalItems { get; set; }
+
+            // Products total number on the one page
+        public int ItemPerPage { get; set; }
+
+            // Current page number
+        public int CurrentPage { get; set; }
+
+            // Pages total number
+        public int TotalPages
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / ItemPerPage); }
+        }
+    }
+}
