016f4fe51fcf913201be214002ff854d9d00ae7e
Alex <licoms1997@gmail.com>
2023-08-17 06:53:58 +0300

    Create IGameRepository interface

---
+++ GameStore.Domain/Abstract/IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameStore.Domain.Entities;
+
+namespace GameStore.Domain.Abstract
+{
+    public interface IGameRepository
+    {
+        IEnumerable<Game> Games { get; }
+    }
+}
