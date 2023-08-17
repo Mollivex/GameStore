ae2ab28b07e7d09c4b9958d33c049a5e8135e132
Alex <licoms1997@gmail.com>
2023-08-17 21:04:24 +0300

    Modify GameContoller.cs

--- GameStore.WebUI/Controllers/GameController.cs
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -1,24 +1,30 @@
 ﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace GameStore.WebUI.Controllers
 {
     public class GameController : Controller
     {
         private IGameRepository repository;
+        public int pageSize = 4;
 
         public GameController(IGameRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Games);
+            return View(repository.Games
+                .OrderBy(game => game.GameId)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize));
         }
     }
 }
