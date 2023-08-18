0cc9f0df7233d5b6ae307080dae7721d184f2487
Alex <licoms1997@gmail.com>
2023-08-18 22:04:55 +0300

    Add navigation controller NavController.cs

---
+++ GameStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        public string Menu()
+        {
+            return "Nav controller testing";
+        }
+    }
+}
