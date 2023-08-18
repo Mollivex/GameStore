bb664222d002f9384861fbda4871e331450b3c6c
Alex <licoms1997@gmail.com>
2023-08-18 20:58:22 +0300

    Modify GameController.cs

--- GameStore.WebUI/Controllers/GameController.cs
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -20,11 +20,12 @@ namespace GameStore.WebUI.Controllers
             repository = repo;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             GamesListViewModel model = new GamesListViewModel
             {
                 Games = repository.Games
+                    .Where(p => category == null || p.Category == category)
                     .OrderBy(game => game.GameId)
                     .Skip((page - 1) * pageSize)
                     .Take(pageSize),
@@ -33,7 +34,8 @@ namespace GameStore.WebUI.Controllers
                     CurrentPage = page,
                     ItemPerPage = pageSize,
                     TotalItems = repository.Games.Count()
-                }
+                },
+                CurrentCategory = category
             };
             return View(model);
         }
