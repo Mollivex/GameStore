ae611836176ea1b95fd7a1b07b42e299463e13c0
Alex <licoms1997@gmail.com>
2023-08-18 20:48:52 +0300

    Modify GamesListViewModel

--- GameStore.WebUI/Models/GameListViewModel.cs
+++ GameStore.WebUI/Models/GameListViewModel.cs
@@ -7,5 +7,7 @@ namespace GameStore.WebUI.Models
     {
         public IEnumerable<Game> Games { get; set; } 
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+
     }
 }
