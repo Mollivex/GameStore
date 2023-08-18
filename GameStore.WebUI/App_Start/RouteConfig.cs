02b89cc9d18274ce2832afa5e98b6f4efba5ae6f
Alex <licoms1997@gmail.com>
2023-08-18 21:53:02 +0300

    Modify routes in RouteConfig.cs

--- GameStore.WebUI/App_Start/RouteConfig.cs
+++ GameStore.WebUI/App_Start/RouteConfig.cs
@@ -13,18 +13,45 @@ namespace GameStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new {controller = "Game", action = "List"}
-
+            routes.MapRoute(null, "",
+                        new
+                        {
+                            controller = "Game",
+                            action = "List",
+                            category = (string)null,
+                            page = 1
+                        }
             );
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Game", action = "List", id = UrlParameter.Optional }
+            routes.MapRoute(name: null, url: "Page{page}",
+                            defaults: new 
+                            { 
+                                controller = "Game", 
+                                action = "List", 
+                                category = (string)null 
+                            },
+                            constraints: new { page = @"\d+" }
             );
+
+            routes.MapRoute(null,"{category}",
+                            new 
+                            { 
+                                controller = "Game", 
+                                action = "List", 
+                                page = 1 
+                            }
+            );
+
+            routes.MapRoute(null,"{category}/Page{page}",
+                            new 
+                            { 
+                                controller = "Game", 
+                                action = "List" 
+                            },
+                            new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
