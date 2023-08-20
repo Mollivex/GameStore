d4189914e52cdee3f2efd67cd89c507ecd113f21
Alex <licoms1997@gmail.com>
2023-08-20 18:36:50 +0300

    Create controller Cart unit tests

---
+++ GameStore.WebUI.Tests/CartTests.cs
@@ -0,0 +1,228 @@
+﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Controllers;
+using GameStore.WebUI.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Items()
+        {
+            // Organization - creating a few test games
+            Game game1 = new Game { GameId = 1, Name = "Game1" };
+            Game game2 = new Game { GameId = 2, Name = "Game2" };
+            Game game3 = new Game { GameId = 3, Name = "Game3" };
+            Game game4 = new Game { GameId = 4, Name = "Game4" };
+
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Action
+            cart.AddItem(game1, 1);
+            cart.AddItem(game2, 1);
+            cart.AddItem(game3, 1);
+            cart.AddItem(game4, 1);
+            List<CartLine> results = cart.Lines.ToList();
+
+            // Statement
+            Assert.AreEqual(results.Count(), 4);
+            Assert.AreEqual(results[0].Game, game1);
+            Assert.AreEqual(results[1].Game, game2);
+            Assert.AreEqual(results[2].Game, game3);
+            Assert.AreEqual(results[3].Game, game4);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            // Organization - creating a few test games
+            Game game1 = new Game { GameId = 1, Name = "Game1" };
+            Game game2 = new Game { GameId = 2, Name = "Game2" };
+            Game game3 = new Game { GameId = 3, Name = "Game3" };
+            Game game4 = new Game { GameId = 4, Name = "Game4" };
+
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Action - adding a few games to the cart
+            cart.AddItem(game1, 2);
+            cart.AddItem(game4, 2);
+            cart.AddItem(game2, 2);
+            cart.AddItem(game3, 1);
+            cart.AddItem(game1, 1);
+            cart.AddItem(game4, 3);
+            List<CartLine> results = cart.Lines.OrderBy(c => c.Game.GameId).ToList();
+
+            // Statement
+            Assert.AreEqual(results.Count(), 4);
+            Assert.AreEqual(results[0].Quantity, 3); // 3 instances are added to cart
+            Assert.AreEqual(results[1].Quantity, 2); // 2 instances are added to cart
+            Assert.AreEqual(results[2].Quantity, 1);
+            Assert.AreEqual(results[3].Quantity, 5); // 6 instances are added to cart
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            // Organization - creating a few test games
+            Game game1 = new Game { GameId = 1, Name = "Game1" };
+            Game game2 = new Game { GameId = 2, Name = "Game2" };
+            Game game3 = new Game { GameId = 3, Name = "Game3" };
+            Game game4 = new Game { GameId = 4, Name = "Game4" };
+
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Organization - adding a few games to the cart
+            cart.AddItem(game1, 2);
+            cart.AddItem(game4, 2);
+            cart.AddItem(game2, 2);
+            cart.AddItem(game3, 1);
+            cart.AddItem(game1, 1);
+            cart.AddItem(game4, 3);
+
+            // Action
+            cart.RemoveLine(game2);
+
+            // Statement
+            Assert.AreEqual(cart.Lines.Where(c => c.Game == game2).Count(), 0);
+            Assert.AreEqual(cart.Lines.Count(), 2);
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            // Organization - creating a few test games
+            Game game1 = new Game { GameId = 1, Name = "Game1", Price = 100 };
+            Game game2 = new Game { GameId = 2, Name = "Game2", Price = 75 };
+            Game game3 = new Game { GameId = 3, Name = "Game3", Price = 50 };
+            Game game4 = new Game { GameId = 4, Name = "Game4", Price = 25 };
+
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Action
+            cart.AddItem(game1, 2);
+            cart.AddItem(game4, 2);
+            cart.AddItem(game2, 2);
+            cart.AddItem(game3, 1);
+            cart.AddItem(game1, 1);
+            cart.AddItem(game4, 3);
+            decimal result = cart.ComputeTotalValue();
+
+            // Statement
+            Assert.AreEqual(result, 625);
+        }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            // Organization - creating a few test games
+            Game game1 = new Game { GameId = 1, Name = "Game1", Price = 100 };
+            Game game2 = new Game { GameId = 2, Name = "Game2", Price = 75 };
+            Game game3 = new Game { GameId = 3, Name = "Game3", Price = 50 };
+            Game game4 = new Game { GameId = 4, Name = "Game4", Price = 25 };
+
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Action
+            cart.AddItem(game1, 2);
+            cart.AddItem(game4, 2);
+            cart.AddItem(game2, 2);
+            cart.AddItem(game3, 1);
+            cart.AddItem(game1, 1);
+            cart.AddItem(game4, 3);
+            cart.Clear();
+
+            // Statement
+            Assert.AreEqual(cart.Lines.Count(), 0);
+        }
+
+        /// <summary>
+        /// Verify adding to cart
+        /// </summary>
+        [TestMethod]
+        public void Can_Add_To_Cart()
+        {
+            // Organization - creating simulated repository
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game{ GameId = 1, Name = "Game1", Category = "Cat1"},
+            }.AsQueryable());
+
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Organization - creating controller
+            CartController controller = new CartController(mock.Object);
+
+            // Action - add game to cart
+            controller.AddToCart(cart, 1, null);
+
+            // Statement
+            Assert.AreEqual(cart.Lines.Count(), 1);
+            Assert.AreEqual(cart.Lines.ToList()[0].Game.GameId, 1);
+        }
+
+        /// <summary>
+        /// After adding game to cart, goes to cart screen redirection
+        /// </summary>
+        [TestMethod]
+        public void Adding_Game_To_Cart_Goes_To_Cart_Screen()
+        {
+            // Organization - creating simulated repository
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game{ GameId = 1, Name = "Game1", Category = "Cat1"},
+            }.AsQueryable());
+
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Organization - creating controller
+            CartController controller = new CartController(mock.Object);
+
+            // Action - add game to cart
+            RedirectToRouteResult result = controller.AddToCart(cart, 2, "myUrl");
+
+            // Statement
+            Assert.AreEqual(result.RouteValues["action"], "Index");
+            Assert.AreEqual(result.RouteValues["returnUrl"], "myUrl");
+        }
+
+        /// <summary>
+        /// Check URL
+        /// </summary>
+        [TestMethod]
+        public void Can_View_Cart_Contents()
+        {
+            // Organization - creating cart
+            Cart cart = new Cart();
+
+            // Organization - creating controller
+            CartController target = new CartController(null);
+
+            // Action - Index() action method call
+            CartIndexViewModel result 
+                = (CartIndexViewModel)target.Index(cart, "myUrl").ViewData.Model;
+
+            // Statement
+            Assert.AreSame(result.Cart, cart);
+            Assert.AreEqual(result.ReturnUrl, "myUrl");
+        }
+    }
+}
