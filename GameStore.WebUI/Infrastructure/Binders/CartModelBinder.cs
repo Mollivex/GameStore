7cf8c243ad60931df6a4042d0529e7747c9f06f0
Alex <licoms1997@gmail.com>
2023-08-20 17:42:04 +0300

    Create model binder in /Infrastructure/Binders/

---
+++ GameStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GameStore.Domain.Entities;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // To get Cart object from session
+            Cart cart = null;
+            if (controllerContext.HttpContext.Session != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+
+            // To create Cart object, if it doesn't spotted in session
+            if(cart == null)
+            {
+                cart = new Cart();
+                if(controllerContext.HttpContext.Session != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+
+            // Return Cart object
+            return cart;
+        }
+    }
+}
