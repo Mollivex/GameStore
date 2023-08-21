244c83bec7e5c0f88791867933044d0d379e0864
Alex <licoms1997@gmail.com>
2023-08-21 06:56:36 +0300

    Create ShippingDetails.cs enitity in /Domain/Entities/

---
+++ GameStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Entities
+{
+    public  class ShippingDetails
+    {
+        [Required(ErrorMessage = "What's your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Insert the first delivery address")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "What's your town")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "What's your country")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
