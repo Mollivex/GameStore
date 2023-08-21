1f75a25a1a8c62ce4cca4b9afd967ff15a8bc010
Alex <licoms1997@gmail.com>
2023-08-21 21:14:49 +0300

    Implementing IOrderProcessor interface (sending messages during making order delivery)

---
+++ GameStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,94 @@
+﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.Domain.Concrete
+{
+    public class EmailSettings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "licoms1997@gmail.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUserName";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = true;
+        public string FileLocation = @"c:\game_store_emails";
+    }
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+         private EmailSettings emailSettings;
+
+        public EmailOrderProcessor(EmailSettings settings)
+        {
+            this.emailSettings = settings;
+        }
+
+        public void ProcessOrder(Cart cart, ShippingDetails shippingInfo)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.ServerName;
+                smtpClient.Port = emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials
+                    = new NetworkCredential(emailSettings.Username, emailSettings.Password);
+
+                if (emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod
+                        = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                    .AppendLine("New order processed")
+                    .AppendLine("---")
+                    .AppendLine("Products:");
+
+                foreach( var line in cart.Lines)
+                {
+                    var subtotal = line.Game.Price * line.Quantity;
+                    body.AppendFormat("{0} x {1} (total: {2:c}",
+                        line.Quantity, line.Game.Name, subtotal);
+                }
+
+                body.AppendFormat("Total price: {0:c}", cart.ComputeTotalValue())
+                    .AppendLine("---")
+                    .AppendLine("Delivery")
+                    .AppendLine(shippingInfo.Name)
+                    .AppendLine(shippingInfo.Line1)
+                    .AppendLine(shippingInfo.Line2 ?? "")
+                    .AppendLine(shippingInfo.Line3 ?? "")
+                    .AppendLine(shippingInfo.City)
+                    .AppendLine(shippingInfo.Country)
+                    .AppendLine("---")
+                    .AppendFormat("Gift wrap: {0}",
+                         shippingInfo.GiftWrap ? "Yes" : "No");
+
+                MailMessage mailMessage = new MailMessage(
+                                        emailSettings.MailFromAddress,  // From whom
+                                        emailSettings.MailToAddress,    // To whom
+                                        "New order sent!",              // Subject
+                                        body.ToString());               // Message body
+
+                if (emailSettings.WriteAsFile)
+                {
+                    mailMessage.BodyEncoding = Encoding.UTF8;
+                }
+
+                smtpClient.Send(mailMessage);
+            }
+        }
+
+    }
+}
