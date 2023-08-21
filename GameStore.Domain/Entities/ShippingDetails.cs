8f2b427f9816d712928d559bf45e16eeb6a37a15
Alex <licoms1997@gmail.com>
2023-08-21 20:16:37 +0300

    Modify Checkout.cshtml view and ShippingDetails.cs entity (code optimization)

--- GameStore.Domain/Entities/ShippingDetails.cs
+++ GameStore.Domain/Entities/ShippingDetails.cs
@@ -13,14 +13,21 @@ namespace GameStore.Domain.Entities
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Insert the first delivery address")]
+        [Display(Name="First address")]
         public string Line1 { get; set; }
+
+        [Display(Name="Second address")]
         public string Line2 { get; set; }
+
+        [Display(Name = "Third address")]
         public string Line3 { get; set; }
 
         [Required(ErrorMessage = "What's your town")]
+        [Display(Name = "City")]
         public string City { get; set; }
 
         [Required(ErrorMessage = "What's your country")]
+        [Display(Name = "Country")]
         public string Country { get; set; }
 
         public bool GiftWrap { get; set; }
