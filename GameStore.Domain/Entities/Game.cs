c4155a43823926cedf3f952b231fb5603599b7ea
Alex <licoms1997@gmail.com>
2023-08-24 21:59:59 +0300

    Modify Game entity for Edit page (using metadata)

--- GameStore.Domain/Entities/Game.cs
+++ GameStore.Domain/Entities/Game.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.Domain.Entities
 {
     public class Game
     {
+        [HiddenInput(DisplayValue = false)]
         public int GameId { get; set; }
+
+        [Display(Name = "Name")]
         public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        [Display(Name="Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Category")]
         public string Category { get; set; }
+
+        [Display(Name = "Price (mdl)")]
         public decimal Price { get; set; }
     }
 }
