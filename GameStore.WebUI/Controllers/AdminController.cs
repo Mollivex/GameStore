af0ba5dffb4949831e09b4a3247ee3e1d4bf86e6
Alex <licoms1997@gmail.com>
2023-08-24 06:51:48 +0300

    Add AdminController.cs

---
+++ GameStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using GameStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IGameRepository repository;
+        public AdminController (IGameRepository repo)
+        {
+            this.repository = repo;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Games);
+        }
+    }
+}
