31de22cbb15ed18feaf84698c95ceb403a667783
Alex <licoms1997@gmail.com>
2023-08-24 19:15:32 +0300

    Add unit tests to AdminTests.cs (content editing)

--- GameStore.WebUI.Tests/Tests/AdminTests.cs
+++ GameStore.WebUI.Tests/Tests/AdminTests.cs
@@ -40,5 +40,56 @@ namespace GameStore.UnitTests
             Assert.AreEqual("Game2", result[1].Name);
             Assert.AreEqual("Game3", result[2].Name);
         }
+
+        [TestMethod]
+        public void Can_Edit_Game()
+        {
+            // Organization - creating simulated repository
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Game1"},
+                new Game { GameId = 2, Name = "Game2"},
+                new Game { GameId = 3, Name = "Game3"},
+                new Game { GameId = 4, Name = "Game4"},
+                new Game { GameId = 5, Name = "Game5"},
+            });
+
+            // Organization - creating controller
+            AdminController controller = new AdminController(mock.Object);
+
+            // Act
+            Game game1 = controller.Edit(1).ViewData.Model as Game;
+            Game game2 = controller.Edit(2).ViewData.Model as Game;
+            Game game3 = controller.Edit(3).ViewData.Model as Game;
+
+            // Assert
+            Assert.AreEqual(1, game1.GameId);
+            Assert.AreEqual(2, game2.GameId);
+            Assert.AreEqual(3, game3.GameId);
+        }
+
+        [TestMethod]
+        public void Cannot_Edit_Nonexistent_Game()
+        {
+            // Organization - creating simulated repository
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Game1"},
+                new Game { GameId = 2, Name = "Game2"},
+                new Game { GameId = 3, Name = "Game3"},
+                new Game { GameId = 4, Name = "Game4"},
+                new Game { GameId = 5, Name = "Game5"},
+            });
+
+            // Organization - creating controller
+            AdminController controller = new AdminController(mock.Object);
+
+            // Act
+            Game result = controller.Edit(6).ViewData.Model as Game;
+
+            //Assert
+        }
     }
 }
