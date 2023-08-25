3f15cd25b44fd0b16546146da97528bea2a2a7c7
Alex <licoms1997@gmail.com>
2023-08-25 07:29:32 +0300

    Modify Game entity (adding annotations)

--- GameStore.Domain/Entities/Game.cs
+++ GameStore.Domain/Entities/Game.cs
@@ -14,16 +14,21 @@ namespace GameStore.Domain.Entities
         public int GameId { get; set; }
 
         [Display(Name = "Name")]
+        [Required(ErrorMessage = "Please, enter game name")]
         public string Name { get; set; }
 
         [DataType(DataType.MultilineText)]
         [Display(Name="Description")]
+        [Required(ErrorMessage = "Please, enter game description")]
         public string Description { get; set; }
 
         [Display(Name = "Category")]
+        [Required(ErrorMessage = "Please, enter game category")]
         public string Category { get; set; }
 
         [Display(Name = "Price (mdl)")]
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please, enter price positive value")]
         public decimal Price { get; set; }
     }
 }
