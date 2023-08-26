8f1f78c6d84311a9424db5730b84b54481ad6bc6
Alex <licoms1997@gmail.com>
2023-08-26 15:14:22 +0300

    Modify Game.cs entity (add ImageData and ImageMimeType fields)

--- GameStore.Domain/Entities/Game.cs
+++ GameStore.Domain/Entities/Game.cs
@@ -26,5 +26,8 @@ namespace GameStore.Domain.Entities
         [Required]
         [Range(0.01, double.MaxValue, ErrorMessage = "Please, enter price positive value")]
         public decimal Price { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
     }
 }
