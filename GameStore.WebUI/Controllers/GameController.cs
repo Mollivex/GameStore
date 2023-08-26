e836ca09fbd8bb73974f54f22392fd54e9f0a06f
Alex <licoms1997@gmail.com>
2023-08-26 16:00:24 +0300

    Modify GameController.cs (implementing the GetImage() action method)

--- GameStore.WebUI/Controllers/GameController.cs
+++ GameStore.WebUI/Controllers/GameController.cs
@@ -41,5 +41,20 @@ namespace GameStore.WebUI.Controllers
             };
             return View(model);
         }
+
+        public FileContentResult GetImage(int gameId)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId = = gameId);
+
+            if (game != null)
+            {
+                return File(game.ImageData, game.ImageMimeType);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
